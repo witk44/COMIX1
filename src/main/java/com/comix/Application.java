89d7a8b3e6a4cf2ae595b55d584d269c3dba7d5f
witk44 <69919362+witk44@users.noreply.github.com>
2022-02-28 10:34:43 -0500

    Fixing merge issues

--- src/main/java/com/comix/Application.java
+++ src/main/java/com/comix/Application.java
@@ -1,22 +1,14 @@
 package com.comix;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.*;
+import jave.sql.Statement;
 
 public class Application {
 
     public static void main(String[] args) {
-        Connection c = null;
-        try {
-           Class.forName("org.postgresql.Driver");
-           c = DriverManager
-              .getConnection("jdbc:postgresql://localhost:5432/Comix",
-              "swen344", "redsox");
-        } catch (Exception e) {
-           e.printStackTrace();
-           System.err.println(e.getClass().getName()+": "+e.getMessage());
-           System.exit(0);
-        }
-        System.out.println("Opened database successfully");
+            System.out.println("Hello world");
+
     }
     
 
