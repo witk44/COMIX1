0390fb53c01a01aaf3ae2bf4888929f8aab55f66
witk44 <69919362+witk44@users.noreply.github.com>
2022-02-28 10:07:57 -0500

    jdbc connection succesful

--- src/main/java/com/comix/Application.java
+++ src/main/java/com/comix/Application.java
@@ -1,9 +1,40 @@
 package com.comix;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Connection c = null;
+        try {
+           Class.forName("org.postgresql.Driver");
+           c = DriverManager
+              .getConnection("jdbc:postgresql://localhost:5432/Comix",
+              "swen344", "redsox");
+        } catch (Exception e) {
+           e.printStackTrace();
+           System.err.println(e.getClass().getName()+": "+e.getMessage());
+           System.exit(0);
+        }
+        System.out.println("Opened database successfully");
     }
+    
+
+// public class PostgreSQLJDBC {
+//    public static void main(String args[]) {
+//       Connection c = null;
+//       try {
+//          Class.forName("org.postgresql.Driver");
+//          c = DriverManager
+//             .getConnection("jdbc:postgresql://localhost:5432/testdb",
+//             "newtest", "555");
+//       } catch (Exception e) {
+//          e.printStackTrace();
+//          System.err.println(e.getClass().getName()+": "+e.getMessage());
+//          System.exit(0);
+//       }
+//       System.out.println("Opened database successfully");
+//    }
+// }
 
 }
