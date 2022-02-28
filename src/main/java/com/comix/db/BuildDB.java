de078129ea5a0ae9f54176dab4011c30fa3f5531
witk44 <69919362+witk44@users.noreply.github.com>
2022-02-28 15:16:34 -0500

    Close to finish csv builder

--- src/main/java/com/comix/db/BuildDB.java
+++ src/main/java/com/comix/db/BuildDB.java
@@ -12,17 +12,40 @@ import com.opencsv.CSVReader;
 
 public class BuildDB {
     public static void Build(Connection connection){
-        String filePath = "C:\\Users\\Connor Witkiewicz\\COMIX1\\comics.csv";
+        String filePath = "comics.csv";
+        try{
+            Statement sql = connection.createStatement();
+            connection.setAutoCommit(true);
+            String sql_stmt = "CREATE TABLE IF NOT EXISTS comix (" + 
+                "id SERIAL,"+
+                "series VARCHAR(255),"+
+                "issue VARCHAR(10),"+
+                "full_title VARCHAR(255),"+
+                "variant VARCHAR(255),"+
+                "publisher VARCHAR(100),"+
+                "release_date VARCHAR(30),"+
+                "format VARCHAR(30),"+
+                "added_date VARCHAR(30),"+
+                "creators VARCHAR(255)"+
+            ");";
+            sql.executeUpdate(sql_stmt);
+            sql.close();
+        }catch(SQLException ie){
+            ie.printStackTrace();
+        }
         
 
         try{
             FileReader fileReader = new FileReader(filePath);
             CSVReader csvReader = new CSVReader(fileReader);
             String [] comic;
+            csvReader.readNext();
+            csvReader.readNext();
+            csvReader.readNext();
             while((comic = csvReader.readNext()) != null){
                 Statement sql = connection.createStatement();
                 // connection.setAutoCommit(true);
-                // String sql_stmt = "INSERT INTO ";
+                // String sql_stmt = "INSERT INTO COMIX VALUES () VALUES comic[1], comic[2]";
                 // sql.executeUpdate(sql_stmt);
                 sql.close();
             }
