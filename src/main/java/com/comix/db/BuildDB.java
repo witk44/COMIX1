10e70473b90aa8851a15d62306aebe4a61761dc9
Drew Pawluckie <drewpawluckie@gmail.com>
2022-02-28 21:02:56 -0500

    Fixed SQL statement for adding parsed csv data

--- src/main/java/com/comix/db/BuildDB.java
+++ src/main/java/com/comix/db/BuildDB.java
@@ -3,9 +3,8 @@ package com.comix.db;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 import javax.print.DocFlavor.STRING;
@@ -16,61 +15,64 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 public class BuildDB {
-    public static void Build(Connection connection){
+    public static void Build(Connection connection) {
         String filePath = "comics.csv";
-        try{
+        try {
             Statement sql = connection.createStatement();
             connection.setAutoCommit(true);
-            String sql_stmt = "CREATE TABLE IF NOT EXISTS comix (" + 
-                "id SERIAL,"+
-                "series VARCHAR(255),"+
-                "issue VARCHAR(10),"+
-                "full_title VARCHAR(255),"+
-                "variant VARCHAR(255),"+
-                "publisher VARCHAR(100),"+
-                "release_date VARCHAR(30),"+
-                "format VARCHAR(30),"+
-                "added_date VARCHAR(30),"+
-                "creators VARCHAR(255)"+
-            ");";
+            String sql_stmt = "CREATE TABLE IF NOT EXISTS comix (" +
+                    "id SERIAL," +
+                    "series VARCHAR(255)," +
+                    "issue VARCHAR(10)," +
+                    "full_title VARCHAR(255)," +
+                    "variant VARCHAR(255)," +
+                    "publisher VARCHAR(100)," +
+                    "release_date VARCHAR(30)," +
+                    "format VARCHAR(30)," +
+                    "added_date VARCHAR(30)," +
+                    "creators VARCHAR(255)" +
+                    ");";
             sql.executeUpdate(sql_stmt);
             sql.close();
-        }catch(SQLException ie){
+        } catch (SQLException ie) {
             ie.printStackTrace();
         }
-        
 
-        try{
-            FileReader fileReader = new FileReader(filePath);
-            CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
-            CSVReader csvReader = new CSVReaderBuilder(fileReader).withCSVParser(parser).build();
-            String [] comic;
-            csvReader.readNext();
-            csvReader.readNext();
-            csvReader.readNext();
-            while((comic = csvReader.readNext()) != null){
-                Statement sql = connection.createStatement();
-                connection.setAutoCommit(true);
-                comic = checkEmpty(comic);
-                String sql_stmt = "INSERT INTO comix VALUES (series, issue, full_title, variant, publisher, release_date, format, added_date, creators) VALUES  (" + comic[0].toString() + "," +  comic[1] + "," + comic[2] + "," + comic[3] + "," + comic[4] + "," + comic[5] + "," + comic[6] + "," + comic[7] + "," + comic[8] + ");";
-                sql.executeUpdate(sql_stmt);
-                // System.out.println(comic[3]);
-                sql.close();
+
+        try {
+            Statement checkTableStatement = connection.createStatement();
+            String checkTableSql = "SELECT COUNT(*) FROM comix";
+            ResultSet result = checkTableStatement.executeQuery(checkTableSql);
+            result.next();
+            int rowCount = result.getInt("COUNT");
+            checkTableStatement.close();
+            result.close();
+
+            if (rowCount == 0) {
+
+                FileReader fileReader = new FileReader(filePath);
+                CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
+                CSVReader csvReader = new CSVReaderBuilder(fileReader).withCSVParser(parser).build();
+                String[] comic;
+                csvReader.readNext();
+                while ((comic = csvReader.readNext()) != null) {
+                    PreparedStatement sql = connection.prepareStatement(
+                            "INSERT INTO comix (series, issue, full_title, variant, publisher, release_date, format, " +
+                                    "added_date, creators) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                    );
+
+                    for (int i = 0; i < 9; i++) {
+                        sql.setString(i + 1, comic[i]);
+                    }
+
+                    sql.executeUpdate();
+                    sql.close();
+                }
+                csvReader.close();
             }
-            csvReader.close();
-        }catch(Exception ie){
-            ie.printStackTrace();
-        }
-    }
-    public static String[] checkEmpty(String[] comic){
-        int x = 0;
-        for(String c : comic){
-            if (c == ""){
-                comic[x] = "empty";
-                x++;
+            } catch(Exception ie){
+                ie.printStackTrace();
             }
-        }
-        return comic;
     }
 }
         
